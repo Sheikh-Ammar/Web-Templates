53fe4bdb05cb28dd4f1c392ecab05371f33d57f9
Sheikh Ammar <sheikhammar568@gmail.com>
2022-10-22 15:35:40 +0500

    First Commit

---
+++ BookShopTemplate/js/myscript.js
@@ -0,0 +1,118 @@
+
+
+
+
+
+// SWIPER JS 
+var swiper = new Swiper(".home-book", {
+    loop:true,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2000,
+      disableOnInteraction: false,
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    },
+  });
+  
+//   var swiper = new Swiper(".featured-slider", {
+//     spaceBetween: 10,
+//     loop:true,
+//     centeredSlides: true,
+//     autoplay: {
+//       delay: 9500,
+//       disableOnInteraction: false,
+//     },
+//     navigation: {
+//       nextEl: ".swiper-button-next",
+//       prevEl: ".swiper-button-prev",
+//     },
+//     breakpoints: {
+//       0: {
+//         slidesPerView: 1,
+//       },
+//       450: {
+//         slidesPerView: 2,
+//       },
+//       768: {
+//         slidesPerView: 3,
+//       },
+//       1024: {
+//         slidesPerView: 4,
+//       },
+//     },
+//   });
+  
+  var swiper = new Swiper(".arrivals-slider", {
+    spaceBetween: 10,
+    loop:true,
+    centeredSlides: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    },
+  });
+  
+  var swiper = new Swiper(".reviews-slider", {
+    spaceBetween: 10,
+    grabCursor:true,
+    loop:true,
+    centeredSlides: true,
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    },
+  });
+  
+   var swiper = new Swiper(".blogs-slider", {
+    spaceBetween: 10,
+    grabCursor:true,
+    loop:true,
+    centeredSlides: true,
+    autoplay: {
+      delay: 9500,
+      disableOnInteraction: false,
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    },
+  });
